0657f1b79c40638be94e487f77c83156c93840ad
Bruno Victor Oliveira Duarte <duartebruno581@gmail.com>
2022-10-26 11:46:08 -0300

    Add files via upload

--- ingresso.c
+++ ingresso.c
@@ -11,14 +11,15 @@ void filme2();
 void filme3();
 void comprar();
 void pagamento();
+void imprimir();
+void gerar();
 
 // ---- Variaváveis Globais ---- //
 
 int quant_bilhetes;
 int horario_sessao;
 int FP;
-int op;
-char sessao[61];
+char nome[61];
 
 // ---- Início do código ---- //
 
@@ -31,17 +32,19 @@ int main(void) {
 }
 
 void menu() {
+    int op;
+
     system("cls");
     printf("================================================\n");
     printf("\t\tCinema da Cidade\n");
     printf("================================================\n");
 
-    printf("SELECIONE UMA DAS OPÇÕES ABAIXO:\n");
-    printf("\t |--> FILMES EM CARTAZ <--| \n");
-    printf("\t[1] | A Bela e a Fera | \n");
-    printf("\t[2] | O mágico de OZ | \n");
-    printf("\t[3] | Se eu fosse você 2 | \n");
-    printf("\t[0] | Sair | \n");
+    printf("\tSELECIONE UMA DAS OPCOES ABAIXO:\n\n");
+    printf("\t |--> FILMES EM CARTAZ <--| \n\n");
+    printf("\t[1] | A Bela e a Fera\n");
+    printf("\t[2] | O Magico de OZ\n");
+    printf("\t[3] | Se eu fosse voce 2\n");
+    printf("\t[0] | Sair\n");
     printf("================================================\n");
     scanf("%d", &op);
     switch (op) {
@@ -54,9 +57,8 @@ void menu() {
         case 3:
             filme3();
             break;
-        
         default:
-            ("Opção inválida!");
+            printf("Opcao invalida!");
             getchar();
             break;
     }
@@ -71,7 +73,9 @@ void filme1() {
     printf("================================================\n");
     printf("Nome: %s\n", nome_filme);
     printf("\nSinopse:\n");
-    printf("Esta é a história de Bela, uma jovem destemida que, em troca da liberdade do pai, oferece a sua companhia a um ser monstruoso que vive num castelo enfeitiçado. O que ela mais tarde vem a perceber é que, debaixo da sua estranha aparência, ele é na realidade um príncipe sob uma maldição...\n");
+    printf("Esta e a historia de Bela, uma jovem destemida que, em troca da liberdade do pai,\n");
+    printf("oferece a sua companhia a um ser monstruoso que vive num castelo enfeiticado. O que ela mais tarde vem a perceber e que,\n");
+    printf("debaixo da sua estranha aparencia, ele e na realidade um principe sob uma maldicao...\n");
     printf("================================================\n");
     printf("\t[1] Comprar ingresso\n");
     printf("\t[2] Retornar ao menu\n");
@@ -84,7 +88,7 @@ void filme1() {
             menu();
             break;
         default:
-            ("Opção inválida!");
+            printf("Opcao invalida!");
             getchar();
             break;
     }
@@ -95,13 +99,15 @@ void filme1() {
 void filme2() {
     int ci;
 
-    char nome_filme[61] = "O magico de OZ";
+    char nome_filme[61] = "O Magico de OZ";
     system("cls");
 
     printf("================================================\n");
     printf("Nome: %s\n", nome_filme);
     printf("\nSinopse:\n");
-    printf("Dorothy e seu cachorro Totó são levados para a terra mágica de Oz quando um ciclone passa pela fazenda de seus avós no Kansas. Eles viajam em direção à Cidade Esmeralda para encontrar o Mago Oz e no caminho encontram um Espantalho, que precisa de um cérebro, um Homem de Lata sem um coração e um Leão Covarde que quer coragem....\n");
+    printf("Dorothy e seu cachorro Toto são levados para a terra magica de Oz quando um ciclone passa pela fazenda de seus avos no Kansas.\n");
+    printf("Eles viajam em direcao a Cidade Esmeralda para encontrar o Mago Oz e no caminho encontram um Espantalho, que precisa de um cerebro,\n");
+    printf("um Homem de Lata sem um coracao e um Leao Covarde que quer coragem....\n");
     printf("================================================\n");
     printf("\t[1] Comprar ingresso\n");
     printf("\t[2] Retornar ao menu\n");
@@ -114,7 +120,7 @@ void filme2() {
             menu();
             break;
         default:
-            ("Opção inválida!");
+            printf("Opcao invalida!");
             getchar();
             break;
     }
@@ -125,13 +131,15 @@ void filme2() {
 void filme3() {
     int ci;
 
-    char nome_filme[61] = "Se eu fosse você 2";
+    char nome_filme[61] = "Se eu fosse voce 2";
     system("cls");
 
     printf("================================================\n");
     printf("Nome: %s\n", nome_filme);
     printf("\nSinopse:\n");
-    printf("Cláudio e Helena estão prestes a se separar. Durante uma discussão, eles trocam de corpos mais uma vez e são obrigados a viver a vida do outro. Para complicar ainda mais a situação, a filha do casal está grávida e não sabe como contar aos pais...\n");
+    printf("Claudio e Helena estao prestes a se separar. Durante uma discussao, \n");
+    printf("eles trocam de corpos mais uma vez e sao obrigados a viver a vida do outro.\n");
+    printf("Para complicar ainda mais a situacao, a filha do casal esta gravida e nao sabe como contar aos pais...\n");
     printf("================================================\n");
     printf("\t[1] Comprar ingresso\n");
     printf("\t[2] Retornar ao menu\n");
@@ -144,7 +152,7 @@ void filme3() {
             menu();
             break;
         default:
-            ("Opção inválida!");
+            printf("Opcao invalida!");
             getchar();
             break;
     }
@@ -154,9 +162,10 @@ void filme3() {
 
 void comprar(nome_filme) {
     int cc;
-    
+    system("cls");
+
     printf("Filme: %s\n", nome_filme);
-    printf("Digite a quantidade de ingressos(Ex: 2, 3...):\n");
+    printf("Digite a quantidade de ingressos (Ex: 2, 3...):\n");
     scanf("%d", &quant_bilhetes);
     printf("================================================\n");
     printf("Selecione a sessao desejada:\n");
@@ -166,7 +175,7 @@ void comprar(nome_filme) {
         printf("[2] Sessao 2 - Sala 2 16:00 3D Dub.\n");
         printf("[3] Sessao 3 - Sala 3 15:30 Dub.\n");
     }
-    else if (nome_filme == "O magico de OZ")
+    else if (nome_filme == "O Magico de OZ")
     {
         printf("[1] Sessao 1 - Sala 2 14:00 Leg.\n");
         printf("[2] Sessao 2 - Sala 2 15:00 3D Dub.\n");
@@ -187,22 +196,94 @@ void comprar(nome_filme) {
     printf("Sessao selecionada: %d\n", horario_sessao);
     printf("\t[1] Confirmar compra\n");
     printf("\t[2] Retornar ao menu\n");
+    printf("================================================\n");
     scanf("%d", &cc);
     switch (cc) {
         case 1:
-            //imprimir();
+            pagamento(nome_filme);
             break;
         case 2:
             menu();
             break;
         default:
-            ("Opção inválida!");
+            printf("Opção inválida!");
             getchar();
             break;
     }
     getchar();
 }
 
-void pagamento(){
-     
+void pagamento(nome_filme){
+    system("cls");
+    printf("================================================\n");
+    printf("Digite seu nome: \n");
+    scanf ("%s", nome);
+    printf("Selecione a forma de pagamento: \n");
+    printf("\t[1] Cartao de debito\n");
+    printf("\t[2] Cartao de credito\n");
+    printf("\t[3] Dinheiro (cedulas)\n");
+    printf("\t[4] Pix\n");
+    printf("================================================\n");
+    scanf("%d", &FP);
+    switch (FP) {
+        case 1:
+            imprimir(nome, FP, nome_filme);
+            break;
+        case 2:
+            imprimir(nome, FP, nome_filme);
+            break;
+        case 3:
+            imprimir(nome, FP, nome_filme);
+            break;
+        case 4:
+            imprimir(nome, FP, nome_filme);
+            break;
+        default:
+            printf("Opção inválida!");
+            getchar();
+            break;
+    }
+    getchar();
+}
+
+void imprimir(nome, FP, nome_filme) {
+    int cf;
+
+    system("cls");
+    printf("================================================\n");
+    printf("Confirme as informacoes abaixo: \n");
+    printf("Nome: %s\n", nome);
+    printf("Forma de pagamento: %d\n", FP);
+    printf("Nome do filme: %s\n", nome_filme);
+    printf("Quantidade de bilhetes: %d\n", quant_bilhetes);
+    printf("Sessao: %d\n", horario_sessao);
+    printf("\t[1] Sim\n");
+    printf("\t[2] Nao\n");
+    scanf("%d", &cf);
+    printf("================================================\n");
+    switch (cf) {
+        case 1:
+            gerar(nome, nome_filme, quant_bilhetes, horario_sessao);
+            break;
+        case 2:
+            menu();
+            break;
+        default:
+            printf("Opção inválida!");
+            getchar();
+            break;
+    }
+}
+
+void gerar(nome, nome_filme, quant_bilhetes, horario_sessao) {
+    system("cls");
+    printf("|-------------------------------------->>\n");
+    printf("| \tINGRESSO DIGITAL\n");
+    printf("| \tNOME:  %s\n", nome );
+    printf("| \tFILME:  %s\n", nome_filme);
+    printf("| \tBILHETE(S):  %d\n", quant_bilhetes);
+    printf("| \tSESSAO:  %d\n", horario_sessao);
+    printf("|-------------------------------------->>\n");
+
+    getchar();
 }
