18ffca772b2de0d41a90dfd0560948b053be5e98
Bruno Victor Oliveira Duarte <duartebruno581@gmail.com>
2022-11-08 17:59:50 -0300

    Add files via upload

--- ingresso.c
+++ ingresso.c
@@ -2,21 +2,8 @@
 #include <stdlib.h>
 #include <locale.h>
 #define quant_bilhetes_MAX 10
-#define valor_int 20
-#define valor_meia 10
-
-// ---- Funções ---- //
-
-void menu();
-void filme1();
-void filme2();
-void filme3();
-void comprar();
-void pagamento();
-void imprimir();
-void gerar();
-
-// ---- Variaváveis Globais ---- //
+#define valor_int 24
+#define valor_meia 12
 
 int codigo_filme;
 int quant_bilhetes;
@@ -24,157 +11,161 @@ int valor_final;
 int valor_bilhetes;
 int horario_sessao;
 int FP;
-char nome[61];
+char nome[15];
 
-// ---- Início do código ---- //
+void menu();
+void filme();
+void pagamento();
+void imprimir();
+void gerar();
+void geracao();
 
+//  Função main, responsável por iniciar o software.
 int main(void) {
-
     menu();
-
     setlocale(LC_ALL, "");
     return 0;
 }
 
+// Função de menu, responsável por disponibilizar os filmes disponíveis e encaminha
+// o código de cada filme como parâmetro para a próxima função.
 void menu() {
     int op;
 
     system("cls");
-    printf("================================================\n");
+    geracao();
     printf("\t\tCinema da Cidade\n");
-    printf("================================================\n");
-
+    geracao();
     printf("\tSELECIONE UMA DAS OPCOES ABAIXO:\n\n");
     printf("\t |--> FILMES EM CARTAZ <--| \n\n");
     printf("\t[1] | A Bela e a Fera\n");
     printf("\t[2] | O Magico de OZ\n");
     printf("\t[3] | Se eu fosse voce 2\n");
     printf("\t[0] | Sair\n");
-    printf("================================================\n");
+    geracao();
     scanf("%d", &op);
     switch (op) {
         case 1:
-            filme1();
+            filme(codigo_filme=1);
             break;
         case 2:
-            filme2();
+            filme(codigo_filme=2);
             break;
         case 3:
-            filme3();
+            filme(codigo_filme=3);
             break;
         default:
             printf("Opcao invalida!");
             getchar();
             break;
     }
+    return;
 }
 
-void filme1() {
+// Função que disponibiliza informações referentes ao filme selecionado.
+// Recebe como parâmetros o código do filme selecionado.
+void filme(codigo_filme) {
     int ci;
-    char nome_filme[61] = "A Bela e a Fera";
-    codigo_filme = 1;
 
     system("cls");
+    if (codigo_filme == 1)
+    {
+        char nome_filme[61] = "A Bela e a Fera";
 
-    printf("================================================\n");
-    printf("Nome: %s\n", nome_filme);
-    printf("\nSinopse:\n");
-    printf("Esta e a historia de Bela, uma jovem destemida que, em troca da liberdade do pai,\n");
-    printf("oferece a sua companhia a um ser monstruoso que vive num castelo enfeiticado. O que ela mais tarde vem a perceber e que,\n");
-    printf("debaixo da sua estranha aparencia, ele e na realidade um principe sob uma maldicao...\n");
-    printf("================================================\n");
-    printf("\t[1] Comprar ingresso\n");
-    printf("\t[2] Retornar ao menu\n");
-    scanf("%d", &ci);
-    switch (ci) {
-        case 1:
-            comprar(nome_filme, codigo_filme);
-            break;
-        case 2:
-            menu();
-            break;
-        default:
-            printf("Opcao invalida!\n");
-            getchar();
-            break;
+        geracao();
+        printf("Nome: %s\n", nome_filme);
+        printf("\nSinopse:\n");
+        printf("Esta e a historia de Bela, uma jovem destemida que, em troca da liberdade do pai,\n");
+        printf("oferece a sua companhia a um ser monstruoso que vive num castelo enfeiticado. O que ela mais tarde vem a perceber e que,\n");
+        printf("debaixo da sua estranha aparencia, ele e na realidade um principe sob uma maldicao...\n");
+        geracao();
+        printf("\t[1] Comprar ingresso\n");
+        printf("\t[2] Retornar ao menu\n");
+        scanf("%d", &ci);
+        switch (ci) {
+            case 1:
+                comprar(nome_filme, codigo_filme);
+                break;
+            case 2:
+                menu();
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                getchar();
+                break;
+        }
+        geracao();
+        getchar();
     }
-    printf("================================================\n");
-    getchar();
+    else if (codigo_filme == 2)
+    {
+        char nome_filme[61] = "O magico de OZ";
+
+        geracao();
+        printf("Nome: %s\n", nome_filme);
+        printf("\nSinopse:\n");
+        printf("Dorothy e seu cachorro Toto sao levados para a terra magica de Oz quando um ciclone passa pela fazenda de seus avos no Kansas.\n");
+        printf("Eles viajam em direcao a Cidade Esmeralda para encontrar o Mago Oz e no caminho encontram um Espantalho, que precisa de um cerebro,\n");
+        printf("um Homem de Lata sem um coracao e um Leao Covarde que quer coragem....\n");
+        geracao();
+        printf("\t[1] Comprar ingresso\n");
+        printf("\t[2] Retornar ao menu\n");
+        scanf("%d", &ci);
+        switch (ci) {
+            case 1:
+                comprar(nome_filme, codigo_filme);
+                break;
+            case 2:
+                menu();
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                getchar();
+                break;
+        }
+        geracao();
+        getchar();
+    }
+    else if (codigo_filme == 3)
+    {
+        char nome_filme[61] = "Se eu fosse voce 2";
+
+        geracao();
+        printf("Nome: %s\n", nome_filme);
+        printf("\nSinopse:\n");
+        printf("Claudio e Helena estao prestes a se separar. Durante uma discussao, \n");
+        printf("eles trocam de corpos mais uma vez e sao obrigados a viver a vida do outro.\n");
+        printf("Para complicar ainda mais a situacao, a filha do casal esta gravida e nao sabe como contar aos pais...\n");
+        geracao();
+        printf("\t[1] Comprar ingresso\n");
+        printf("\t[2] Retornar ao menu\n");
+        scanf("%d", &ci);
+        switch (ci) {
+            case 1:
+                comprar(nome_filme, codigo_filme);
+                break;
+            case 2:
+                menu();
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                getchar();
+                break;
+        }
+        geracao();
+        getchar();
+    }
+    return;
 }
 
-void filme2() {
-    int ci;
-    char nome_filme[61] = "O Magico de OZ";
-    codigo_filme = 2;
-
-    system("cls");
-
-    printf("================================================\n");
-    printf("Nome: %s\n", nome_filme);
-    printf("\nSinopse:\n");
-    printf("Dorothy e seu cachorro Toto são levados para a terra magica de Oz quando um ciclone passa pela fazenda de seus avos no Kansas.\n");
-    printf("Eles viajam em direcao a Cidade Esmeralda para encontrar o Mago Oz e no caminho encontram um Espantalho, que precisa de um cerebro,\n");
-    printf("um Homem de Lata sem um coracao e um Leao Covarde que quer coragem....\n");
-    printf("================================================\n");
-    printf("\t[1] Comprar ingresso\n");
-    printf("\t[2] Retornar ao menu\n");
-    scanf("%d", &ci);
-    switch (ci) {
-        case 1:
-            comprar(nome_filme, codigo_filme);
-            break;
-        case 2:
-            menu();
-            break;
-        default:
-            printf("Opcao invalida!\n");
-            getchar();
-            break;
-    }
-    printf("================================================\n");
-    getchar();
-}
-
-void filme3() {
-    int ci;
-    char nome_filme[61] = "Se eu fosse voce 2";
-    codigo_filme = 3;
-
-    system("cls");
-
-    printf("================================================\n");
-    printf("Nome: %s\n", nome_filme);
-    printf("\nSinopse:\n");
-    printf("Claudio e Helena estao prestes a se separar. Durante uma discussao, \n");
-    printf("eles trocam de corpos mais uma vez e sao obrigados a viver a vida do outro.\n");
-    printf("Para complicar ainda mais a situacao, a filha do casal esta gravida e nao sabe como contar aos pais...\n");
-    printf("================================================\n");
-    printf("\t[1] Comprar ingresso\n");
-    printf("\t[2] Retornar ao menu\n");
-    scanf("%d", &ci);
-    switch (ci) {
-        case 1:
-            comprar(nome_filme, codigo_filme);
-            break;
-        case 2:
-            menu();
-            break;
-        default:
-            printf("Opcao invalida!\n");
-            getchar();
-            break;
-    }
-    printf("================================================\n");
-    getchar();
-}
-
+// Função que permite a seleção de ingressos, quantidades e sessão desejada.
+// Recebe como parâmetros o nome do filme e o código do filme selecionado.
 void comprar(nome_filme, codigo_filme) {
     int cc;
     int rt;
     int ig;
 
     system("cls");
-
     printf("Filme: %s\n", nome_filme);
     printf("Selecione se o ingresso e inteiro ou meia-entrada: \n");
     printf("[1] Inteiro\n");
@@ -210,8 +201,8 @@ void comprar(nome_filme, codigo_filme) {
         }
         getchar();
     }else
-    {
-        printf("================================================\n");
+    { 
+        geracao();
         printf("Selecione a sessao desejada:\n");
         if (codigo_filme == 1) {
             printf("[1] Sessao 1 - Sala 2 15:00 Leg.\n");
@@ -230,13 +221,13 @@ void comprar(nome_filme, codigo_filme) {
         }
  
         scanf("%d", &horario_sessao);
-        printf("================================================\n");
+        geracao();
         printf("Confirme as informacoes abaixo:\n");
         printf("Quantidade de ingressos: %d\n", quant_bilhetes);
         printf("Sessao selecionada: %d\n", horario_sessao);
         printf("\t[1] Confirmar compra\n");
         printf("\t[2] Retornar ao menu\n");
-        printf("================================================\n");
+        geracao();
         scanf("%d", &cc);
         switch (cc) {
             case 1:
@@ -250,14 +241,17 @@ void comprar(nome_filme, codigo_filme) {
                 getchar();
                 break;
         }
-    } 
+    }
+    return;
 }
 
+// Função que permite a seleção da forma de pagamento e imprime o valor final da compra.
+// Recebe como parâmetros o nome do filme.
 void pagamento(nome_filme){
-    int valor_final = quant_bilhetes * valor_bilhetes;
+    int valor_final = (quant_bilhetes * valor_bilhetes);
 
     system("cls");
-    printf("================================================\n");
+    geracao();
     printf("Digite seu nome: \n");
     scanf ("%s", nome);
     printf("Valor final de sua compra: %d\n", valor_final);
@@ -266,7 +260,7 @@ void pagamento(nome_filme){
     printf("\t[2] Cartao de credito\n");
     printf("\t[3] Dinheiro (cedulas)\n");
     printf("\t[4] Pix\n");
-    printf("================================================\n");
+    geracao();
     scanf("%d", &FP);
     switch (FP) {
         case 1:
@@ -287,16 +281,37 @@ void pagamento(nome_filme){
             break;
     }
     getchar();
+    return;
 }
 
+// Função que permite confirmar as informações ou retonar ao menu.
+// Recebe como parâmetros o nome do usuário, forma de pagamento selecionada,
+// nome do filme e o valor final da compra.
 void imprimir(nome, FP, nome_filme, valor_final) {
     int cf;
 
+    // if (FP = 1)
+    // {
+    //     char Pag[61] = "Cartao de debito";
+    // }
+    // else if (FP = 2)
+    // {
+    //     char Pag[61] = "Cartao de credito";
+    // }
+    // else if (FP = 3)
+    // {
+    //     char Pag[61] = "Dinheiro (cedula)";
+    // }
+    // else if (FP = 4)
+    // {
+    //     char Pag[61] = "Pix";;
+    // }
+    
     system("cls");
-    printf("================================================\n");
+    geracao();
     printf("Confirme as informacoes abaixo: \n");
     printf("Nome: %s\n", nome);
-    printf("Forma de pagamento: %d\n", FP);
+    printf("Forma de pagamento: %s\n", FP);
     printf("Nome do filme: %s\n", nome_filme);
     printf("Quantidade de bilhetes: %d\n", quant_bilhetes);
     printf("Sessao: %d\n", horario_sessao);
@@ -304,7 +319,7 @@ void imprimir(nome, FP, nome_filme, valor_final) {
     printf("\t[1] Sim\n");
     printf("\t[2] Nao\n");
     scanf("%d", &cf);
-    printf("================================================\n");
+    geracao();
     switch (cf) {
         case 1:
             gerar(nome, nome_filme, quant_bilhetes, horario_sessao, valor_final);
@@ -317,33 +332,26 @@ void imprimir(nome, FP, nome_filme, valor_final) {
             getchar();
             break;
     }
+    return;
 }
 
+// Função que recebe como parâmetro o nome do usuário, nome do filme, quantidade de bilhetes,
+// sessão selecionada e valor final a ser pago.
 void gerar(nome, nome_filme, quant_bilhetes, horario_sessao, valor_final) {
     system("cls");
     printf("|-------------------------------------->>\n");
-    printf("| \tINGRESSO DIGITAL\n");
+    printf("| \tINGRESSO DIGITAL\n"); 
     printf("| \tNOME:  %s\n", nome );
     printf("| \tFILME:  %s\n", nome_filme);
     printf("| \tBILHETE(S):  %d\n", quant_bilhetes);
     printf("| \tSESSAO:  %d\n", horario_sessao);
     printf("| \tVALOR:  %d\n", valor_final);
-    printf("|-------------------------------------->>\n");
+    printf("|-------------------------------------->>\n"); 
+    return;
+}
 
-
-    FILE *arq;
-
-    arq = fopen("ingresso.txt", "w");
-    if (arq != NULL)
-    {
-        fprintf(arq, "%s" "%s" "%d" "%d" "%d", nome, nome_filme, quant_bilhetes, horario_sessao, valor_final);
-    }
-    else
-    { 
-        printf("ERRO!\n");
-    }
-
-    fclose(arq);
-
-    getchar(); 
+// Workaround para gerar conteúdo visual
+void geracao() {
+    printf("================================================\n");
+    return;
 }
